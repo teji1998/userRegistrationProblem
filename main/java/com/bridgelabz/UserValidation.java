e8f5c01057392f29b31f91374eee728a28ac34ee
“teji1998” <tejasvinirpk@gmail.com>
2020-08-12 07:27:44 +0530

    Added:Function to check valid password.

--- main/java/com/bridgelabz/UserValidation.java
+++ main/java/com/bridgelabz/UserValidation.java
@@ -4,8 +4,9 @@ import java.util.regex.Pattern;
 
 public class UserValidation {
 	private static final String NAME_PATTERN="^[A-Z][A-Za-z]{2,}";
-	private static final String EmailId_PATTERN="^[0-9a-z]+[+_.-]?[0-9a-z]+[@][0-9a-z]+[.][a-z]{2,}[.]?[a-z]+$";
+	private static final String EMAIL_ID_PATTERN="^[0-9a-z]+[+_.-]?[0-9a-z]+[@][0-9a-z]+[.][a-z]{2,}[.]?[a-z]+$";
 	private static final String PHONE_NUMBER_PATTERN="^[0-9]{2}[ ][0-9]{10}";
+	private static final String PASSWORD_PATTERN="^[0-9A-Za-z]{8,}";
 
 	public boolean validateFirstName(String firstName) {
 		Pattern pattern=Pattern.compile(NAME_PATTERN);
@@ -18,7 +19,7 @@ public class UserValidation {
 	}
 
 	public boolean validateEmailId(String emailid) {
-		Pattern pattern=Pattern.compile(EmailId_PATTERN);
+		Pattern pattern=Pattern.compile(EMAIL_ID_PATTERN);
 		return pattern.matcher(emailid).matches();
 	}
 
@@ -26,5 +27,10 @@ public class UserValidation {
 		Pattern pattern=Pattern.compile(PHONE_NUMBER_PATTERN);
 		return pattern.matcher(phoneNumber).matches();
 
+	}
+
+	public boolean validatePassword(String passsword) {
+		Pattern pattern=Pattern.compile(PASSWORD_PATTERN);
+		return pattern.matcher(passsword).matches();
 	}
 }
