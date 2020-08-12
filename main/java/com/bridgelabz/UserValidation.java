60c78cedfa6a6315e58336d1da0aa4a787e2c023
“teji1998” <tejasvinirpk@gmail.com>
2020-08-12 10:51:50 +0530

    Refactored:Changes in the variable names.

--- main/java/com/bridgelabz/UserValidation.java
+++ main/java/com/bridgelabz/UserValidation.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class UserValidation {
 	private static final String NAME_PATTERN="^[A-Z][A-Za-z]{2,}";
-	private static final String EmailId_PATTERN="^[0-9a-z]+[+_.-]?[0-9a-z]+[@][0-9a-z]+[.][a-z]{2,}[.]?[a-z]+$";
+	private static final String EMAIL_ID_PATTERN="^[0-9a-z]+[+_.-]?[0-9a-z]+[@][0-9a-z]+[.][a-z]{2,}[.]?[a-z]+$";
 
 	public boolean validateFirstName(String firstName) {
 		Pattern pattern=Pattern.compile(NAME_PATTERN);
@@ -17,7 +17,7 @@ public class UserValidation {
 	}
 
 	public boolean validateEmailId(String emailid) {
-		Pattern pattern=Pattern.compile(EmailId_PATTERN);
+		Pattern pattern=Pattern.compile(EMAIL_ID_PATTERN);
 		return pattern.matcher(emailid).matches();
 	}
 }
