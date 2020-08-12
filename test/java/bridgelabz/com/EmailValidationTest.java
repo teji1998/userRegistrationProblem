8fa13a232f229cd8b61c6f882d40020cb993b8df
“teji1998” <tejasvinirpk@gmail.com>
2020-08-12 13:32:58 +0530

    Refactored:Added other test cases.

--- test/java/bridgelabz/com/EmailValidationTest.java
+++ test/java/bridgelabz/com/EmailValidationTest.java
@@ -12,47 +12,46 @@ import java.util.Collection;
 
 @RunWith(Parameterized.class)
 public class EmailValidationTest {
-		public String email;
-		public boolean result;
-		public UserValidation userValidation;
+	public String email;
+	public boolean result;
+	public UserValidation userValidation;
 
-		public EmailValidationTest(String email,boolean result){
-			this.email=email;
-			this.result=result;
-		}
-		@Before
-		public void initialize(){
-			userValidation=new UserValidation();
-		}
-		@Parameterized.Parameters
-		public static Collection input(){
-			return Arrays.asList(new Object[][] {{"abc@yahoo.com",true},
-					{"abc-100@yahoo.com",true},
-					{"abc.100@yahoo.com",true},
-					{"abc111@abc.com",true},
-					{"abc-100@abc.net",true},
-					{"abc.100@abc.com.au",true},
-					{"abc@1.com",true},
-					{"abc@gmail.com.com",true},
-					{"abc+100@gmail.com",true},
-					{"abc",false},
-					{"abc@.com.my",false},
-					{"abc123@gmail.a",false},
-					{"abc123@.com",false},
-					{"abc123@.com.com",false},
-					{".abc@abc.com",false},
-					{"abc()*@gmail.com",false},
-					{"abc@%*.com",false},
-					{"abc..2002@gmail.com",false},
-					{"abc.@gmail.com",false},
-					{"abc@abc@gmail.com",false},
-					{"abc@gmail.com.1a",false},
-					{"abc@gmail.com.aa.au",false}});
-		}
+	public EmailValidationTest(String email,boolean result){
+		this.email=email;
+		this.result=result;
+	}
+	@Before
+	public void initialize(){
+		userValidation=new UserValidation();
+	}
+	@Parameterized.Parameters
+	public static Collection input(){
+		return Arrays.asList(new Object[][] {{"abc@yahoo.com",true},
+				{"abc-100@yahoo.com",true},
+				{"abc.100@yahoo.com",true},
+				{"abc111@abc.com",true},
+				{"abc-100@abc.net",true},
+				{"abc.100@abc.com.au",true},
+				{"abc@1.com",true},
+				{"abc@gmail.com.com",true},
+				{"abc+100@gmail.com",true},
+				{"abc",false},
+				{"abc@.com.my",false},
+				{"abc123@gmail.a",false},
+				{"abc123@.com",false},
+				{"abc123@.com.com",false},
+				{".abc@abc.com",false},
+				{"abc()*@gmail.com",false},
+				{"abc@%*.com",false},
+				{"abc..2002@gmail.com",false},
+				{"abc.@gmail.com",false},
+				{"abc@abc@gmail.com",false},
+				{"abc@gmail.com.1a",false},
+				{"abc@gmail.com.aa.au",false}});
+	}
 
-		@Test
-		public void emailCheck() {
-			Assert.assertEquals(result,userValidation.validateEmailId(email));
-		}
+	@Test
+	public void emailValidity() {
+		Assert.assertEquals(result,userValidation.validateEmailId(email));
+	}
 }
-
