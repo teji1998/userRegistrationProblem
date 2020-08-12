f5d23782f61eccfd8460fcd368015f46ced8c817
“teji1998” <tejasvinirpk@gmail.com>
2020-08-12 07:54:50 +0530

    Added:Condition for atleast one number.

--- main/java/com/bridgelabz/UserValidation.java
+++ main/java/com/bridgelabz/UserValidation.java
@@ -6,7 +6,8 @@ public class UserValidation {
 	private static final String NAME_PATTERN="^[A-Z][A-Za-z]{2,}";
 	private static final String EMAIL_ID_PATTERN="^[0-9a-z]+[+_.-]?[0-9a-z]+[@][0-9a-z]+[.][a-z]{2,}[.]?[a-z]+$";
 	private static final String PHONE_NUMBER_PATTERN="^[0-9]{2}[ ][0-9]{10}";
-	private static final String PASSWORD_PATTERN="(?=.*[A-Z].*)[A-Za-z0-9]{8,}";
+	private static final String PASSWORD_PATTERN="(?=.*[A-Z].*)(?=.*[0-9].*)[A-Za-z0-9@#$%^&*+]{8,}";
+
 
 	public boolean validateFirstName(String firstName) {
 		Pattern pattern=Pattern.compile(NAME_PATTERN);
