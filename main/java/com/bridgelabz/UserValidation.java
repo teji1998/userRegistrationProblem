4661ecf2de7672e07f30e51f14945af7848070a0
“teji1998” <tejasvinirpk@gmail.com>
2020-08-12 06:47:25 +0530

    Added:Function to check email address.

--- main/java/com/bridgelabz/UserValidation.java
+++ main/java/com/bridgelabz/UserValidation.java
@@ -3,10 +3,21 @@ package com.bridgelabz;
 import java.util.regex.Pattern;
 
 public class UserValidation {
-	private static final String FIRST_NAME_PATTERN="^[A-Z][A-Za-z]{2,}";
+	private static final String NAME_PATTERN="^[A-Z][A-Za-z]{2,}";
+	private static final String EmailId_PATTERN="^[0-9a-z]+[+_.-]?[0-9a-z]+[@][0-9a-z]+[.][a-z]{2,}[.]?[a-z]+$";
 
 	public boolean validateFirstName(String firstName) {
-		Pattern pattern=Pattern.compile(FIRST_NAME_PATTERN);
+		Pattern pattern=Pattern.compile(NAME_PATTERN);
 		return pattern.matcher(firstName).matches();
+	}
+
+	public boolean validateLastName(String lastName) {
+		Pattern pattern=Pattern.compile(NAME_PATTERN);
+		return pattern.matcher(lastName).matches();
+	}
+
+	public boolean validateEmailId(String emailid) {
+		Pattern pattern=Pattern.compile(EmailId_PATTERN);
+		return pattern.matcher(emailid).matches();
 	}
 }
