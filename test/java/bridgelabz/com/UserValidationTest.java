25a81392759ac4783455f14d5eecfb3c158fc7ee
“teji1998” <tejasvinirpk@gmail.com>
2020-08-19 15:03:22 +0530

    Refactored:Corrected Indentations.

--- test/java/bridgelabz/com/UserValidationTest.java
+++ test/java/bridgelabz/com/UserValidationTest.java
@@ -12,6 +12,7 @@ public class UserValidationTest {
 		boolean result = userValidation.validateFirstName("Tejaswini");
 		Assert.assertTrue(result);
 	}
+
 	@Test
 	public void givenFirstName_WhenShort_ShouldReturnFalse() {
 		UserValidation userValidation = new UserValidation();
@@ -22,55 +23,56 @@ public class UserValidationTest {
 	@Test
 	public void givenLastName_WhenProper_ShouldReturnTrue() {
 		UserValidation userValidation = new UserValidation();
-		boolean result=userValidation.validateLastName("Kulkarni");
+		boolean result = userValidation.validateLastName("Kulkarni");
 		Assert.assertTrue(result);
 	}
 
 	@Test
 	public void givenLastName_WhenShort_ShouldReturnFalse() {
-		UserValidation userValidation=new UserValidation();
-		boolean result=userValidation.validateFirstName("Ku");
+		UserValidation userValidation = new UserValidation();
+		boolean result = userValidation.validateFirstName("Ku");
 		Assert.assertFalse(result);
 	}
+
 	@Test
 	public void givenEmailId_WhenProper_ShouldReturnTrue() {
-		UserValidation userValidation=new UserValidation();
-		boolean result=userValidation.validateEmailId("tejasvinirpk@gmail.com");
+		UserValidation userValidation = new UserValidation();
+		boolean result = userValidation.validateEmailId("tejasvinirpk@gmail.com");
 		Assert.assertTrue(result);
 	}
 
 	@Test
 	public void givenEmailId_WhenShort_ShouldReturnFalse() {
-		UserValidation userValidation=new UserValidation();
-		boolean result=userValidation.validateEmailId("@yahoo.com");
+		UserValidation userValidation = new UserValidation();
+		boolean result = userValidation.validateEmailId("@yahoo.com");
 		Assert.assertFalse(result);
 	}
 
 	@Test
 	public void givenPhoneNumber_WhenProper_ShouldReturnTrue() {
-		UserValidation userValidation=new UserValidation();
-		boolean result=userValidation.validatePhoneNumber("91 9920275347");
+		UserValidation userValidation = new UserValidation();
+		boolean result = userValidation.validatePhoneNumber("91 9920275347");
 		Assert.assertTrue(result);
 	}
 
 	@Test
 	public void givenPhoneNumber_WhenNotProper_ShouldReturnFalse() {
-		UserValidation userValidation=new UserValidation();
-		boolean result=userValidation.validatePhoneNumber("919920275347");
+		UserValidation userValidation = new UserValidation();
+		boolean result = userValidation.validatePhoneNumber("919920275347");
 		Assert.assertFalse(result);
 	}
 
 	@Test
 	public void givenPassword_WhenProper_ShouldReturnTrue() {
-		UserValidation userValidation=new UserValidation();
-		boolean result=userValidation.validatePassword("Tejaswini+1998");
+		UserValidation userValidation = new UserValidation();
+		boolean result = userValidation.validatePassword("Tejaswini+1998");
 		Assert.assertTrue(result);
 	}
 
 	@Test
 	public void givenPassword_WhenNotProper_ShouldReturnFalse() {
-		UserValidation userValidation=new UserValidation();
-		boolean result=userValidation.validatePassword("teju");
+		UserValidation userValidation = new UserValidation();
+		boolean result = userValidation.validatePassword("teju");
 		Assert.assertFalse(result);
 	}
 }
