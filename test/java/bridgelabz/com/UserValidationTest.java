59e279906607c57d3df9179f06353d2fd04ec9c2
“teji1998” <tejasvinirpk@gmail.com>
2020-08-19 15:01:48 +0530

    Refactored:Used assertTrue or assertFalse instead of assertEquals.

--- test/java/bridgelabz/com/UserValidationTest.java
+++ test/java/bridgelabz/com/UserValidationTest.java
@@ -6,71 +6,72 @@ import org.junit.Test;
 
 public class UserValidationTest {
 
-	UserValidation userValidation = new UserValidation();
-	boolean result = userValidation.validateFirstName("Tejaswini");@Test
+	@Test
 	public void givenFirstName_WhenProper_ShouldReturnTrue() {
-		Assert.assertEquals(true, result);
+		UserValidation userValidation = new UserValidation();
+		boolean result = userValidation.validateFirstName("Tejaswini");
+		Assert.assertTrue(result);
 	}
-
 	@Test
 	public void givenFirstName_WhenShort_ShouldReturnFalse() {
 		UserValidation userValidation = new UserValidation();
 		boolean result = userValidation.validateFirstName("Te");
-		Assert.assertEquals(false, result);
+		Assert.assertFalse(result);
 	}
 
 	@Test
 	public void givenLastName_WhenProper_ShouldReturnTrue() {
-		UserValidation userValidation=new UserValidation();
+		UserValidation userValidation = new UserValidation();
 		boolean result=userValidation.validateLastName("Kulkarni");
-		Assert.assertEquals(true,result);
+		Assert.assertTrue(result);
 	}
 
 	@Test
 	public void givenLastName_WhenShort_ShouldReturnFalse() {
 		UserValidation userValidation=new UserValidation();
 		boolean result=userValidation.validateFirstName("Ku");
-		Assert.assertEquals(false,result);
+		Assert.assertFalse(result);
 	}
 	@Test
 	public void givenEmailId_WhenProper_ShouldReturnTrue() {
 		UserValidation userValidation=new UserValidation();
 		boolean result=userValidation.validateEmailId("tejasvinirpk@gmail.com");
-		Assert.assertEquals(true,result);
+		Assert.assertTrue(result);
 	}
 
 	@Test
 	public void givenEmailId_WhenShort_ShouldReturnFalse() {
 		UserValidation userValidation=new UserValidation();
 		boolean result=userValidation.validateEmailId("@yahoo.com");
-		Assert.assertEquals(false,result);
+		Assert.assertFalse(result);
 	}
 
 	@Test
 	public void givenPhoneNumber_WhenProper_ShouldReturnTrue() {
 		UserValidation userValidation=new UserValidation();
 		boolean result=userValidation.validatePhoneNumber("91 9920275347");
-		Assert.assertEquals(true,result);
+		Assert.assertTrue(result);
 	}
 
 	@Test
 	public void givenPhoneNumber_WhenNotProper_ShouldReturnFalse() {
 		UserValidation userValidation=new UserValidation();
 		boolean result=userValidation.validatePhoneNumber("919920275347");
-		Assert.assertEquals(false,result);
+		Assert.assertFalse(result);
 	}
 
 	@Test
 	public void givenPassword_WhenProper_ShouldReturnTrue() {
 		UserValidation userValidation=new UserValidation();
 		boolean result=userValidation.validatePassword("Tejaswini+1998");
-		Assert.assertEquals(true,result);
+		Assert.assertTrue(result);
 	}
+
 	@Test
 	public void givenPassword_WhenNotProper_ShouldReturnFalse() {
 		UserValidation userValidation=new UserValidation();
 		boolean result=userValidation.validatePassword("teju");
-		Assert.assertEquals(false,result);
+		Assert.assertFalse(result);
 	}
 }
 
